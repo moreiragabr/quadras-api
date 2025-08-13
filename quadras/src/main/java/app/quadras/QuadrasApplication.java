889151f8e722b1effbdffa1844d4f49d70c7a510
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2025-08-13 19:56:59 -0300

    primeiro commit

---
+++ quadras/src/main/java/app/quadras/QuadrasApplication.java
@@ -0,0 +1,13 @@
+package app.quadras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadrasApplication.class, args);
+	}
+
+}
