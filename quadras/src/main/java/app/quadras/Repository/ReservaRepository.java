10a1ed87acfd4ab025140959bae84e838acd24f8
Gustavo Maniero <gustavomaniero@hotmail.com>
2025-08-17 20:48:59 -0300

    fix:api exclusao de classe

--- quadras/src/main/java/app/quadras/Repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package app.quadras.Repository;
-
-import app.quadras.Entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
